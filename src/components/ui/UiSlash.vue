7305e847fb149f37946d637ed6d8e794aa4f4dbd
rosmis <rosmis123@gmail.com>
2023-03-18 14:01:49 +0100

    home page with carousel onboarding still in draft

---
+++ src/components/ui/UiSlash.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="skew-container skew top-0 right-0 bottom-0 w-20 absolute">
+        <div class="h-full skew"></div>
+    </div>
+</template>
+
+<style scoped>
+.skew-container {
+    transform: skewY(-11deg) translateY(-10%);
+    background-color: red;
+}
+.skew {
+    transform: skewY(11deg);
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,62 @@
 <template>
-  <ui-page screen>
-    <ui-wrapper>
-      <ui-level class="flex-col">
-        <DashboardHeader />
-        <HomeHeader />
-        <HomeOffer />
-      </ui-level>
-    </ui-wrapper>
-    <HomeTrainers />
-  </ui-page>
+    <ui-page screen class="relative">
+        <UiSlash />
+        <ui-wrapper>
+            <n-carousel class="carousel">
+                <img class="carousel-img" src="../assets/asset_home_page.png" />
+                <img class="carousel-img" src="../assets/asset_home_page.png" />
+                <img class="carousel-img" src="../assets/asset_home_page.png" />
+                <img class="carousel-img" src="../assets/asset_home_page.png" />
+
+                <template #dots="{ total, currentIndex, to }">
+                    <ul class="custom-dots">
+                        <li
+                            v-for="index of total"
+                            :key="index"
+                            :class="{
+                                ['is-active']: currentIndex === index - 1,
+                            }"
+                        />
+                    </ul>
+                </template>
+            </n-carousel>
+        </ui-wrapper>
+    </ui-page>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { NCarousel } from "naive-ui";
+</script>
+
+<style scoped>
+.carousel {
+    position: static !important;
+}
+.custom-dots {
+    display: flex;
+    margin: 0;
+    padding: 0;
+    position: absolute;
+    bottom: 2em;
+    left: 50%;
+    transform: translateX(-50%);
+    cursor: default !important;
+}
+
+.custom-dots li {
+    display: inline-block;
+    width: 12px;
+    height: 10px;
+    margin: 0 3px;
+    border-radius: 4px;
+    cursor: default !important;
+    background-color: #a09f9f;
+    transition: width 0.3s, background-color 0.3s cubic-bezier(0.4, 0, 0.2, 1);
+    cursor: pointer;
+}
+
+.custom-dots li.is-active {
+    width: 40px;
+    background: #100e51;
+}
+</style>
