87a1327a910a04eb22dcf1a4d9071a38d1ecaad6
rosmis <rosmis123@gmail.com>
2023-03-19 13:30:35 +0100

    created ui components

--- src/components/ui/UiSlash.vue
+++ src/components/ui/UiSlash.vue
@@ -1,15 +1,11 @@
 <template>
-    <div class="skew-container skew top-0 right-0 bottom-0 w-20 absolute">
-        <div class="h-full skew"></div>
-    </div>
+    <div
+        class="h-full bg-[#1a098c1a] skew top-0 right-55 bottom-0 w-70 absolute"
+    ></div>
 </template>
 
 <style scoped>
-.skew-container {
-    transform: skewY(-11deg) translateY(-10%);
-    background-color: red;
-}
 .skew {
-    transform: skewY(11deg);
+    transform: skewX(-40deg);
 }
 </style>
