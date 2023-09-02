33da5b74fe46fabe43a329376d0f7c79e3360a3e
rosmis <rosmis123@gmail.com>
2023-09-02 19:34:30 +0200

    update on list to show user's UUIDs

--- src/components/list/UserRowList.vue
+++ src/components/list/UserRowList.vue
@@ -4,6 +4,7 @@
             <p class="text-white">{{ user.first_name }} {{ user.last_name }}</p>
             <UiLevel>
                 <p class="text-white">{{ user.email }}</p>
+                <p class="text-white">{{ user.id }}</p>
 
                 <UiButton
                     v-if="!copied"
@@ -26,11 +27,11 @@ const props = defineProps<{
     user: Partial<User>;
 }>();
 
-const userEmail = props.user.email;
+const userId = props.user.id;
 
 const { copy, copied } = useClipboard();
 
 const copyEmail = () => {
-    copy(userEmail);
+    copy(userId);
 };
 </script>
